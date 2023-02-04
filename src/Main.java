f4d12f818e4ba4284a179537d5a5e98afea18c38
Dao Tran <dat6914@g.rit.edu>
2023-02-04 14:49:34 -0500

    Most of beginning

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Storage Manager!");
         if (args.length != 3) {
             System.err.println("Usage: <db_loc> <page_size> <buffer_size>");
         }
@@ -19,43 +18,33 @@ public class Main {
         //if there is then restart that database
         //else, create a new database at given location with pagesize and buffersize
 
-        displayCommand();
-        System.out.println("Enter the command: ");
-        String input = scanner.nextLine();
-        String[] optionArr = input.split(" ");
-        if (input.equals("display schema")){
-            displaySchema();
-        } else if (input.equals("quit")) {
-            quitProgram();
-        } else if (optionArr[0].equals("display") && optionArr[1].equals("info") && optionArr.length == 3) {
-            displayInfo(optionArr[2]);
-        } else if (optionArr[0].equals("insert") && optionArr[1].equals("info") && optionArr[3].equals("values") && optionArr.length >= 5) {
-            //Tuple = parse string aaaaa
-            insertTuplesIntoTable(optionArr[2], optionArr[4]);
-        } else if (optionArr[0].equals("select") && optionArr[1].equals("*") && optionArr[2].equals("from") && optionArr.length == 4) {
-            selectFrom(optionArr[3]);
-        } else if (optionArr[0].equals("create") && optionArr[1].equals("tables") && optionArr.length > 3) {
-            //Tuple = parse string
-            createTable(optionArr[2], optionArr[3]);
-        } else {
-            System.err.println("Not a valid command");
+        System.out.println("Database is available now!");
+
+        while(true) {
+            displayCommand();
+            System.out.println("Enter the command: ");
+            String input = scanner.nextLine();
+            String[] optionArr = input.split(" ");
+            if (input.equals("display schema")) {
+                displaySchema();
+            } else if (input.equals("quit program")) {
+                quitProgram();
+                break;
+            } else if (optionArr[0].equals("display") && optionArr[1].equals("info") && optionArr.length == 3) {
+                displayInfo(optionArr[2]);
+            } else if (optionArr[0].equals("insert") && optionArr[1].equals("info") && optionArr[3].equals("values") && optionArr.length >= 5) {
+                //Tuple = parse string aaaaa
+                insertTuplesIntoTable(optionArr[2], optionArr[4]);
+            } else if (optionArr[0].equals("select") && optionArr[1].equals("*") && optionArr[2].equals("from") && optionArr.length == 4) {
+                selectRecordsFromTable(optionArr[3]);
+            } else if (optionArr[0].equals("create") && optionArr[1].equals("tables") && optionArr.length > 3) {
+                //Tuple = parse string
+                createTable(optionArr[2], optionArr[3]);
+            } else {
+                System.err.println("It is not a valid command.");
+            }
         }
 
-
-    }
-
-    public static void displayCommand() {
-        System.out.println("Database is now available!");
-        System.out.println("List of commands:");
-        System.out.println("display schema");
-        System.out.println("display info <name>");
-        System.out.println("select * from <name>");
-        System.out.println("insert into <name> values <tuples>");
-        System.out.println("create table <name> (");
-        System.out.println("      <attr_name1> <attr_type1> primarykey, ");
-        System.out.println("      <attr_name2> <attr_type2>,...");
-        System.out.println("      <attr_nameN> <attr_typeN>  ); ");
-        System.out.println("quit");
 
     }
 
@@ -63,7 +52,7 @@ public class Main {
 
     }
 
-    public static void selectFrom(String tableName) {
+    public static void selectRecordsFromTable(String tableName) {
 
     }
 
@@ -72,14 +61,37 @@ public class Main {
     }
 
     public static void displayInfo(String tableName) {
-
+        System.out.println("Table name: ");
+        System.out.println("Table schema: ");
+        System.out.println("Number of pages: ");
+        System.out.println("Number of records: ");
     }
 
     public static void displaySchema() {
-
+        System.out.println("Database location: " );
+        System.out.println("Page size: ");
+        System.out.println("Buffer size: ");
+        System.out.println("Table schema: ");
     }
 
     public static void quitProgram() {
+        //terminate the database
+        System.out.println("Quit program safely!");
+    }
+
+    public static void displayCommand() {
+        System.out.println("----------------------------------------------");
+        System.out.println("Storage Manager!");
+        System.out.println("List of commands:");
+        System.out.println("    display schema");
+        System.out.println("    display info <name>");
+        System.out.println("    select * from <name>");
+        System.out.println("    insert into <name> values <tuples>");
+        System.out.println("    create table <name> (");
+        System.out.println("        <attr_name1> <attr_type1> primarykey,");
+        System.out.println("        <attr_name2> <attr_type2>,...");
+        System.out.println("        <attr_nameN> <attr_typeN>  );");
+        System.out.println("    quit program");
 
     }
 }
