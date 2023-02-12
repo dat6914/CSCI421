100de844d87f15c98c5abd047322377df149c4e0
Jiawei Liu <jwl5643@g.rit.edu>
2023-02-12 16:04:38 -0500

    add size

--- src/Record.java
+++ src/Record.java
@@ -3,20 +3,13 @@ import java.util.ArrayList;
 public class Record {
       private ArrayList<Object> values_list = new ArrayList<>();
       private String primaryKey;
+      private int size;
 
       public Object getObject(int i) {
             return values_list.get(i);
       }
 
-
       public static byte[] convertRecordTextToBinary() {
             return null;
-      }
-
-      public static void start(){
-            System.out.println("Hello");
-      }
-      public void size(){
-            System.out.println("size = 0");
       }
 }
