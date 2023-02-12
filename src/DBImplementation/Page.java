ad1b326666cea930afa1c58de2ced199b04da9cc
Lu Li <ll6474@g.rit.edu>
2023-02-12 16:53:28 -0500

    changed record object

--- src/DBImplementation/Page.java
+++ src/DBImplementation/Page.java
@@ -2,14 +2,16 @@ package DBImplementation;
 
 import java.lang.reflect.Array;
 import java.util.*;
+import DBImplementation.Record;
 
 public class Page {
     private int CurrentSize;
 
     private int PageID;
+    private Record records;
 
     private ArrayList<String> Record_PrimaryKey = new ArrayList<>();
 
-    private  Map<String,String> RecordObj = new HashMap<>();
+    private  Map<String,Record> RecordObj = new HashMap<>();
 
 }
