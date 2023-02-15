7e3c922d54d86f1ea637b1bace1cd7c7ce373718
Lu Li <ll6474@g.rit.edu>
2023-02-14 19:19:31 -0500

    Update Page Structure

--- src/DBImplementation/Page.java
+++ src/DBImplementation/Page.java
@@ -4,38 +4,33 @@ import java.awt.*;
 import java.lang.reflect.Array;
 import java.util.*;
 import DBImplementation.Record;
+import DBImplementation.Pointer;
 
 public class Page {
-    // Current Page Size
-    private int CurrentSize;
     // Current Page ID
     private int PageID;
-
     private ArrayList<Pointer> pointerList = new ArrayList<>();
     // Record objects
     private ArrayList<Record> recordList = new ArrayList<>();
 
+    // byte array of records
+    private byte recordByte[] = new byte[4096];
+
+    // number of record
+    private int numOfRecord;
+
     // page Constructor
-    public Page(int CurrentSize, int PageID){
-        this.CurrentSize = CurrentSize;
+    public Page(int PageID, int numOfRecord, ArrayList<Pointer> pointerList, byte[] recordByte){
         this.PageID = PageID;
+        this.numOfRecord = numOfRecord;
+        this.pointerList = pointerList;
+        this.recordByte = recordByte;
     }
 
-    // gets the sum of all the records in this page
-    public void updateCurrentSize(){
-        int sum = 0;
-        for (Record records : recordList){
-            sum += records.getSize();
-        }
-        CurrentSize = sum;
-    }
 
     public void insertPointerToPointerList(Pointer pointer){
         pointerList.add(pointer);
     }
-
-
-
 
     public ArrayList<Record> getRecordList(){
         return recordList;
@@ -49,11 +44,6 @@ public class Page {
         return PageID;
     }
 
-    // gets the sum of all the records in this page
-    public int getCurrentSize(){
-        return CurrentSize;
-    }
-
     public boolean recordExist(String primaryKey){
         int index = 0;
         for (Record record : recordList){
@@ -64,11 +54,12 @@ public class Page {
         return false;
     }
 
-    public boolean PageFull(Record CurrRecSize){
-        int PageMax = 4096;
-        int PageSize = getCurrentSize();
-        return (CurrRecSize.getSize() + PageSize) >= PageMax;
-    }
 
+    public void insertRec(Pointer pointer, byte[] record){
+        for (int i = this.recordByte.length; i > 0; i--) {
+
+
+        }
+    }
 
 }
